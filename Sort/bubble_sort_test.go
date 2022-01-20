8ede5b0f35c69f29f562427c1f0fad6b53d935c6
zhangjiulong-ps <zhangjiulong-ps@360.cn>
2022-01-20 11:23:51 +0800

    commit

--- Sort/bubble_sort_test.go
+++ Sort/bubble_sort_test.go
@@ -16,11 +16,11 @@ func TestBubbleSort(t *testing.T) {
 	}{
 		{name: "one", args: struct{ list []int }{list: []int{5, 19, 1, 89, 8, 14, 16, 49, 25, 4, 6, 13}}},
 		{name:"two", args: struct{ list []int }{list: []int{7,6,1,2,3,4,5}}},
-		{name:"three", args: struct{ list []int }{list: []int{0, 2, 9, 8, 7, 10, 8, 100}}},
+		{name:"three", args: struct{ list []int }{list: []int{0, 2, 9, 8, 7, 10, 8, 100, -1}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			MergeSort(tt.args.list)
+			QuickSort(tt.args.list)
 		})
 		fmt.Println(tt.args.list)
 	}
---
+++ Sort/quick_sort.go
@@ -0,0 +1,35 @@
+package Sort
+
+func QuickSort(list []int) {
+	if len(list) <= 1 {
+		return
+	}
+	qSort(list, 0, len(list))
+}
+
+func qSort(list []int, begin int, end int) { //左闭右开区间
+	if begin >= end || begin+1 >= end { //区间为空或者区间只有一个元素
+		return
+	}
+	mid := begin + ((end - begin) >> 1)
+	x := list[mid]
+
+	l := begin
+	i := begin
+	r := end - 1
+	//三路切分
+	for i <= r {
+		if list[i] < x {
+			list[l], list[i] = list[i], list[l]
+			l++
+			i++
+		} else if list[i] == x {
+			i++
+		} else {
+			list[i], list[r] = list[r], list[i]
+			r--
+		}
+	}
+	qSort(list, begin, l)
+	qSort(list, i, end)
+}
