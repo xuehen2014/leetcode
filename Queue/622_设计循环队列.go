a91475920644e2005c9ca93a5697b63a649af715
= <=>
2023-05-08 11:07:04 +0800

    c

--- Queue/622_设计循环队列.go
+++ Queue/622_设计循环队列.go
@@ -1,49 +1,47 @@
 package Queue
 
+import "fmt"
+
 /*
  * https://leetcode-cn.com/problems/design-circular-queue/
  */
 
 type MyCircularQueue struct {
-	data []int
-	used int
-	front int
-	rear int
-	capacity int
+	data     []int //循环队列的存储空间
+	used     int   //已经使用的元素个数
+	front    int   //第一个元素所在位置-队首
+	rear     int   //rear是EnQueue可在存放的位置,待插入的位置, [front,rear)
+	capacity int   //循环队列最多可以存放的元素个数
 }
-
 
 func Constructor(k int) MyCircularQueue {
 	return MyCircularQueue{
 		capacity: k,
-		front : 0,
-		rear : 0,
-		used : 0,
-		data: make([]int, k, k),
+		front:    0,
+		rear:     0,
+		used:     0,
+		data:     make([]int, k, k),
 	}
 }
-
 
 func (this *MyCircularQueue) EnQueue(value int) bool {
 	if this.IsFull() {
 		return false
 	}
 	this.data[this.rear] = value
-	this.rear = (this.rear+1)%this.capacity
+	this.rear = (this.rear + 1) % this.capacity
 	this.used++
 	return true
 }
-
 
 func (this *MyCircularQueue) DeQueue() bool {
 	if this.IsEmpty() {
 		return false
 	}
-	this.front = (this.front+1)%this.capacity
+	this.front = (this.front + 1) % this.capacity
 	this.used--
 	return true
 }
-
 
 func (this *MyCircularQueue) Front() int {
 	if this.IsEmpty() {
@@ -53,25 +51,35 @@ func (this *MyCircularQueue) Front() int {
 	return res
 }
 
-
 func (this *MyCircularQueue) Rear() int {
 	if this.IsEmpty() {
 		return -1
 	}
-	res := (this.rear-1+this.capacity)%this.capacity
+	res := (this.rear - 1 + this.capacity) % this.capacity
 	return this.data[res]
 }
-
 
 func (this *MyCircularQueue) IsEmpty() bool {
 	return this.used == 0
 }
 
-
 func (this *MyCircularQueue) IsFull() bool {
 	return this.used == this.capacity
 }
 
+//打印循环队列的所有元素
+func (this *MyCircularQueue) List() {
+	if this.IsEmpty() {
+		return
+	}
+	var list []int
+	p := this.front
+	for i := 1; i <= this.used; i++ {
+		list = append(list, this.data[p])
+		p = (p + 1) % this.capacity
+	}
+	fmt.Println(list)
+}
 
 /**
  * Your MyCircularQueue object will be instantiated and called as such:
--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"github.com/xuehen2014/leetcode/Sort"
-)
+import "github.com/xuehen2014/leetcode/Queue"
 
 func main()  {
-	list := []int{1,2,3,5}
-	Sort.BubbleSort(list)
-	fmt.Println(list)
+	//list := []int{1,2,3,5}
+	//Sort.BubbleSort(list)
+	//fmt.Println(list)
+	circularQueue := Queue.Constructor(5)
+	circularQueue.EnQueue(1)
+	circularQueue.EnQueue(2)
+	circularQueue.EnQueue(3)
+	circularQueue.EnQueue(4)
+	circularQueue.EnQueue(5)
+	circularQueue.List()
+	circularQueue.DeQueue()
+	circularQueue.DeQueue()
+	circularQueue.List()
+	circularQueue.EnQueue(6)
+	circularQueue.List()
+	circularQueue.DeQueue()
+	circularQueue.List()
 }
